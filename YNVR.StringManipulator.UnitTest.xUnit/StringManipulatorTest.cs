09ca6a8d3b25e513fcae15dffa2702f1aadbf367
Natanaelito77 <natanelvilleda@outlook.es>
2024-10-11 11:23:48 -0600

    Agregar archivos de proyecto.

---
+++ YNVR.StringManipulator.UnitTest.xUnit/StringManipulatorTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YNVR.StringManipulator.UnitTest.xUnit
+{
+    public class StringManipulatorTest
+    {
+    
+        private readonly StringManipulator _stringManipulator;
+
+        public StringManipulatorTest()
+        {
+            _stringManipulator = new StringManipulator();
+        }
+
+        [Fact]
+        public void ReverseString_InputIsHello_ShouldReturnOlleh()
+        {
+
+            var input = "Hola";
+
+            var result = _stringManipulator.ReverseString(input);
+
+
+            Assert.Equal("aloH", result);
+        }
+
+        [Fact]
+        public void RemoveSpaces_InputHasSpaces_ShouldRemoveAllSpaces()
+        {
+
+            var input = "H o l a";
+
+
+            var result = _stringManipulator.RemoveSpaces(input);
+
+
+            Assert.Equal("Hola", result);
+        }
+    }
+}
+
---
+++ YNVR.StringManipulator/StringManipulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YNVR.StringManipulator
+{
+    public class StringManipulator
+    {
+            public string ReverseString(string input)
+            {
+                char[] charArray = input.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+
+            public string RemoveSpaces(string input)
+            {
+                return input.Replace(" ", "");
+            }
+    }
+}
+
